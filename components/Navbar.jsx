7957bd7694b1110d73e25c19eb503b057201123d
Glen Calica <gcalica@myseneca.ca>
2022-10-05 18:35:39 -0400

    styled navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,28 @@
+import Link from "next/link";
+
 export default function Navbar() {
    return (
       <>
-         <h1 className="text-2xl font-bold">Frameworking Ministries</h1>
-         <ul>
-            <li>Articles</li>
-            <li>About</li>
-         </ul>
+         <div className="px-12 py-4 flex justify-between">
+            <h1 className="text-2xl">Frameworking Ministries</h1>
+            <ul className="flex">
+               <li className="px-2">
+                  <Link as={"../articles"} href={"../pages/articles"}>
+                     Articles
+                  </Link>
+               </li>
+               <li className="px-2">
+                  <Link as={"../about"} href={"../pages/about"}>
+                     About
+                  </Link>
+               </li>
+               <li className="px-2">
+                  <Link as={"../give"} href={"../pages/give"}>
+                     Give
+                  </Link>
+               </li>
+            </ul>
+         </div>
       </>
    );
 }
