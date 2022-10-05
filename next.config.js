463123f6799d87b9010d778850ff5d137c64b66f
Glen Calica <gcalica@myseneca.ca>
2022-10-05 12:37:08 -0400

    .md files in _posts now convert to blog posts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-   reactStrictMode: true,
-   swcMinify: true,
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//    reactStrictMode: true,
+//    swcMinify: true,
+// };
+
+module.exports = {
+   webpack5: true,
+   webpack: (config) => {
+      config.resolve.fallback = {
+         fs: false,
+         path: false,
+      };
+
+      return config;
+   },
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
